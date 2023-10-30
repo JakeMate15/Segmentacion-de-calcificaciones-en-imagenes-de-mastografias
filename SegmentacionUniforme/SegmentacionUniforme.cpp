75609526b520be6d257fed3ae87933330951a0be
JakeMate15 <erik.mofe51@gmail.com>
2023-10-30 10:06:32 -0600

    correccion ecualizacion uniforme

--- SegmentacionUniforme/SegmentacionUniforme.cpp
+++ SegmentacionUniforme/SegmentacionUniforme.cpp
@@ -1,78 +1,58 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-typedef long double ld;
+
+vector<int> histogramaEqualization(const vector<int>& histograma, long long tamImg) {
+    int grises = 255;
+
+    vector<int> cdf(grises);
+
+    // Calcula la función de distribución acumulada (CDF)
+    cdf[0] = histograma[0];
+    for (int i = 1; i < grises; ++i) {
+        cdf[i] = cdf[i - 1] + histograma[i];
+    }
+
+    vector<int> ecualizacion(grises);
+
+    // Aplica la ecualización de histogramaa
+    for (int i = 0; i < grises; ++i) {
+        ecualizacion[i] = round((long double)(cdf[i]) / tamImg * (grises - 1));
+    }
+
+    return ecualizacion;
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(0);
+    cout << fixed << setprecision(10);
 
     int n, m;
     cin >> n >> m;
 
     //Lectura de la imagen y cálculo de sus frecuencias
     int a[n][m];
-    map<int, int> his;
+    vector<int> histogramaa(256);
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
             cin >> a[i][j];
-            his[a[i][j]]++;
-            //cout << a[i][j] << "\t";
+            histogramaa[a[i][j]]++;
         }
-        //cout << "\n";
+        
     }
 
-    //cout << "\n\nHistograma original\n";
+    vector<int> nvosValores = histogramaEqualization(histogramaa, n * m);
+    
 
-    //Impresión de las frecuencias de cada valor de gris
-    for(auto [g, rep]: his) {
-        cout << g << " " << rep << "\n";
-    }
-
-    //cout << "\n";
-
-    //Cálculo de la probabilidad de aparición de cada nivel de gris
-    vector<ld> pg(256);
-    for(int i = 0; i <= 255; i++) {
-        pg[i] = (1.0) * his[i] / (n * m);
-        //cout << "P(" << i << "): " << pg[i] << "\n";
-    }
-
-    //cout << "\n";
-
-    //Cálculo de gmax y gmin
-    int gMin = ((*his.begin()).first), gMx = (*prev(his.end())).first;
-
-    //Cálculo de los nuevos nivel de gris
-    vector<int> nuevosValores(256);
-    for(int i = 0; i <= 255; i++) {
-        nuevosValores[i] = (gMx - gMin) * pg[i] + gMin;
-        //cout << "nV(" << i << "): " << nuevosValores[i] << "\n";
-    }
-
-    int nvo = 0;
-    map<int,int> nvoHist;
-    //Reemplazar con los nuevos valores
+    cout << n << " " << m << "\n";
+    int nuevaImg[n][m];
     for(int i = 0; i < n; i++) {
         for(int j = 0; j < m; j++) {
-            a[i][j] = nuevosValores[a[i][j]];
-            nvoHist[a[i][j]]++;
-        }
-    }
-
-    //cout << "Nueva imagen\n";
-    cout << n << " " << m << '\n';
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            cout << a[i][j] << "\t";
+            nuevaImg[i][j] = nvosValores[a[i][j]];
+            cout << nuevaImg[i][j] << "\t";
         }
         cout << "\n";
     }
 
-    //cout << "Nuevo histograma\n";
-    //Impresión de las frecuencias de cada valor de gris del nuevo
-    //Histograma
-    for(auto [g, rep]: nvoHist) {
-        cout << g << " " << rep << "\n";
-    }
 }
